158e45f81b8efd7a12f94c882319850218da0034
Manimohan05 <manimohan621@gmail.com>
2026-02-26 21:55:53 +0530

    database

--- backend/backend/src/main/resources/db/migration/V15__Drop_Unique_Constraint_On_Fee_Exemptions.sql
+++ backend/backend/src/main/resources/db/migration/V15__Drop_Unique_Constraint_On_Fee_Exemptions.sql
@@ -1,3 +1,3 @@
 -- V15__Drop_Unique_Constraint_On_Fee_Exemptions.sql
 -- Drop the remaining unique constraint on student_id in fee_exemptions
-ALTER TABLE fee_exemptions DROP CONSTRAINT IF EXISTS ukbb5a5k5to62hiv07p5f35uerx;
+ALTER TABLE fee_exemptions DROP CONSTRAINT IF EXISTS fee_exemptions_student_id_key;
